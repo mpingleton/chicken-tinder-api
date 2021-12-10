4c33392585a667fc1ab39c2e9172943a148b83b1
Michael Pingleton <mnpingleton@gmail.com>
2021-12-10 16:29:25 -0600

    Added image urls to getting all restaurants.

--- src/controllers/restaurant.controller.js
+++ src/controllers/restaurant.controller.js
@@ -3,11 +3,21 @@ const restaurantService = require('../services/restaurant.service');
 const getAllRestaurants = async (req, res) => {
   const restaurants = await restaurantService.getAllRestaurants();
 
-  const responseData = restaurants.map((restaurant) => ({
-    id: restaurant.id,
-    name: restaurant.name,
-    location: restaurant.location,
-  }));
+  const promises = [];
+  const responseData = restaurants.map((restaurant) => {
+    const restaurantData = {
+      id: restaurant.id,
+      name: restaurant.name,
+      location: restaurant.location,
+    };
+    promises.push(restaurantService.getRestaurantPhotos(restaurantData.id)
+      .then((photos) => {
+        restaurantData.photos = photos.map((photo) => ({ url: photo.photo_url }));
+      }));
+
+    return restaurantData;
+  });
+  await Promise.all(promises);
 
   res.send(200, responseData);
 };
